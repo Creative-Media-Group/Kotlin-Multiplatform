eef11c566ac3dd893bc7ff55316476eeb3f29c91
tct123 <42028373+tct123@users.noreply.github.com>
2024-08-09 00:55:08 +0200

    .

--- androidApp/src/main/java/com/example/kotlinmultiplatform/android/MainActivity.kt
+++ androidApp/src/main/java/com/example/kotlinmultiplatform/android/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    GreetingView(Greeting().greet())
+                    GreetingView("Test")
                 }
             }
         }
--- shared/src/commonMain/kotlin/com/example/kotlinmultiplatform/Platform.kt
+++
@@ -1,7 +0,0 @@
-package com.example.kotlinmultiplatform
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
